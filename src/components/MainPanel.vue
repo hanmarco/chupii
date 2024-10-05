c14adca813c0631d09d884ff8288e0fd30777554
Marco <hsn103@gmail.com>
2024-10-05 23:26:26 +0900

    Change color

--- src/components/MainPanel.vue
+++ src/components/MainPanel.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app-bar :elevation="4" color="red">
-    <v-app-bar-title>T'choupi</v-app-bar-title>
+    <v-app-bar-title @click="openUrl('/')">T'choupi</v-app-bar-title>
   </v-app-bar>
-  <VSheet>
-    <v-btn v-for="x in urlList" :key="x" @click="openUrl(x.url)">{{x.label}}</v-btn>
+  <VSheet class="ma-2">
+    <v-btn v-for="x in urlList" :key="x" @click="openUrl(x.url)" block class="mt-2" variant="tonal" color="red">{{x.label}}</v-btn>
   </VSheet>
 </template>
 
