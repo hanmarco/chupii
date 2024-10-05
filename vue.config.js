0ec64891c6ddb97c8f68ceef1b66947e23cf906d
Marco <hsn103@gmail.com>
2024-10-05 22:55:22 +0900

    Add publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/chupi/' : '/',
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
