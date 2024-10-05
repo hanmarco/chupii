85aab4a2b154b79181fc9a823b0a621929f6e0f1
Marco <hsn103@gmail.com>
2024-10-05 23:10:45 +0900

    Add button

--- src/components/MainPanel.vue
+++ src/components/MainPanel.vue
@@ -2,6 +2,9 @@
   <v-app-bar :elevation="4" color="red">
     <v-app-bar-title>T'choupi</v-app-bar-title>
   </v-app-bar>
+  <VSheet>
+    <v-btn v-for="x in list" :key="x">{{x}}</v-btn>
+  </VSheet>
 </template>
 
 <script>
@@ -9,7 +12,7 @@ export default {
   name: "MainPanel",
   created() {},
   data() {
-    return {};
+    return {list: [{label:"오디오북", url:"http://naver.com"}]};
   },
   props: {},
   methods: {},
