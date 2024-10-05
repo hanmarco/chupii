dc7d836dcc30138c850d0c5a274dbe6bfeef5fb2
Marco <hsn103@gmail.com>
2024-10-06 02:26:56 +0900

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <v-main>
       <MainPanel :urlList="urlList"/>
     </v-main>
+    
+  <v-footer>
+    <div class="mb-2 text-center w-100">
+      {{ new Date().getFullYear() }} — Made by <strong>Da-young</strong> & <strong>Do-young</strong>
+    </div>
+  </v-footer>
   </v-app>
 </template>
 
