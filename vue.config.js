735b4c7470137257de6ee78044a313fc3749540c
Marco <hsn103@gmail.com>
2024-10-06 23:09:16 +0900

    Fix manifest for PWA

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/chupi/' : '/',
+  pwa: {
+    name: 'Chupi',
+    workboxOptions: {
+      navigateFallback: '/chupi/index.html',
+    }
+  },
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {
