1ac279aa3d7cd17816bac3f3007c31e590cdb5cd
hanmarco <hsn103@gmail.com>
2024-10-10 01:12:47 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/chupi/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/chupii/' : '/',
   pwa: {
     name: 'Chupi',
     workboxOptions: {
